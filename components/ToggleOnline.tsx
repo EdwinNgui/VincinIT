87b214f861c724554be34f150c00f9f109574b03
fiorittoev <fiorittoev@gmail.com>
2024-09-28 14:42:07 -0400

    Added online toggle component

---
+++ components/ToggleOnline.tsx
@@ -0,0 +1,33 @@
+import 
+{
+    connect,publish,subscribe,
+    checkBluetoothPermission,
+    checkBluetoothAvailability,
+    useNearbyErrorCallback,
+    addOnErrorListener,
+    disconnect,
+    unsubscribe,
+    unpublish,
+}from 'react-native-google-nearby-messages';
+import Constants from 'expo-constants'
+
+const EnableConnections = async () =>
+{
+    const removeListener = addOnErrorListener((kind, message) => console.error(`${kind}: ${message}`));
+    const disconnect = await connect({apiKey:"AIzaSyC_gFI0z52xCw69ACbPfKWXOoZXUL-7MSs"});
+    const unpublish = await publish('hello !');
+    const unsubscribe = await subscribe(
+    (m) => {
+        console.log(`new message found: ${m}`);
+    },
+    (m) => {
+        console.log(`message lost: ${m}`);
+    });
+};
+const DisableConnections = async () =>
+{
+    disconnect();
+    unsubscribe();
+    unpublish();
+}
+export {EnableConnections,DisableConnections}
