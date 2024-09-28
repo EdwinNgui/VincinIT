8e614c28871eeade8d477143a06a1f32f803d034
fiorittoev <fiorittoev@gmail.com>
2024-09-28 16:19:42 -0400

    Realm configuration

--- realmConfig.ts
+++ realmConfig.ts
@@ -1,7 +1,9 @@
-import { App } from 'realm-web';
-
-const app = new App({
+import {createRealmContext} from '@realm/react';
+import * as Realm from 'realm'
+// Define your MongoDB Realm app configuration
+const appConfig = {
   id: "66f85cf08f32cc1e5ad2b6ea", // Your MongoDB Realm App ID
-});
-
-export default app;
+};
+// Use the `Realm` instance
+const realmApp = new Realm.App(appConfig);
+export default realmApp;
