346185aae29e1e04d2a850b83d25a658568142de
Richard Glosser <glosserr@msu.edu>
2024-09-28 18:59:53 -0400

    Connect basic layout
    Danny Devito is a personality not involved with our company but he seemed kinda cool so we gave him a cameo

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/app` | `/(tabs)/connect` | `/(tabs)/message` | `/(tabs)/profile` | `/_sitemap` | `/app` | `/connect` | `/login` | `/message` | `/profile` | `/register`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/connect` | `/(tabs)/message` | `/(tabs)/profile` | `/(tabs)\` | `/(tabs)\connect` | `/(tabs)\profile` | `/_sitemap` | `/connect` | `/login` | `/message` | `/profile` | `/register`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
