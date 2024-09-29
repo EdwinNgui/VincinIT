42f467fa592b4dd8fca21480e034fff355a4e827
Richard Glosser <glosserr@msu.edu>
2024-09-28 23:14:37 -0400

    Added Groq API Access

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/connect` | `/(tabs)\_layout` | `/(tabs)\connect` | `/(tabs)\message` | `/(tabs)\profile` | `/..\components\ToggleOnline` | `/_sitemap` | `/connect` | `/login` | `/register`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/connect` | `/(tabs)/message` | `/(tabs)/profile` | `/(tabs)\_layout` | `/(tabs)\connect` | `/(tabs)\message` | `/(tabs)\profile` | `/..\components\GatherNearbyUsers` | `/..\components\ToggleOnline` | `/_sitemap` | `/connect` | `/login` | `/message` | `/profile` | `/register`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
