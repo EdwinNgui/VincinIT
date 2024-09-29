51a2d26337d175ab71349ffdeab1d81798ad19c2
Edwin <edwin.nguim@gmail.com>
2024-09-28 20:18:33 -0400

    Home Page and Login Styles

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/app` | `/(tabs)/connect` | `/(tabs)/profile` | `/..\components\GatherNearbyUsers` | `/..\components\ToggleOnline` | `/_sitemap` | `/app` | `/connect` | `/login` | `/profile` | `/register`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/connect` | `/_sitemap` | `/connect` | `/login` | `/register`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
