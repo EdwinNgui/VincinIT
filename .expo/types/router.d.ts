c478ecff18283b107cbce62251bdb3a3b60018b1
fiorittoev <fiorittoev@gmail.com>
2024-09-28 21:31:53 -0400

    Location services

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,11 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-<<<<<<< HEAD
       StaticRoutes: `/` | `/(tabs)` | `/(tabs)/connect` | `/(tabs)/message` | `/(tabs)/profile` | `/_sitemap` | `/connect` | `/login` | `/message` | `/profile` | `/register`;
-=======
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/connect` | `/(tabs)/profile` | `/(tabs)\_layout` | `/(tabs)\connect` | `/(tabs)\message` | `/(tabs)\profile` | `/_sitemap` | `/connect` | `/login` | `/profile` | `/register`;
->>>>>>> 8e30b5cbd7eb49633353629818ceadff480dc108
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
